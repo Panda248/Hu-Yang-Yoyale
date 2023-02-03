ff5ee495ada61996bb534ebd6252725110f65369
Panda248 <p4nda248@gmail.com>
2023-02-02 21:08:21 -0500

    created player movement and basic enemy ai
    enemy ai chases player when in fov

---
+++ Enemy.gd
@@ -0,0 +1,48 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var velocity = 50
+export var health = 10
+
+var motion = Vector2()
+
+var state = IDLE
+
+enum	{
+	IDLE, CHASE
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#State machine
+	match state:
+		CHASE:
+			_chase(delta)
+	pass
+
+func _chase(delta):
+	var player = get_parent().get_node("Player")
+	look_at(player.position)
+	motion = move_and_collide((player.position - position).normalized() * delta * velocity)
+	pass
+
+
+func _on_FOV_body_entered(body):
+	if(body == get_parent().get_node("Player")):
+		state = CHASE
+	pass
+
+
+func _on_FOV_body_exited(body):
+	if(body == get_parent().get_node("Player")):
+		state = IDLE
+	pass # Replace with function body.
+
