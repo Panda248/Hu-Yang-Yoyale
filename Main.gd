b19609b121c8b278171f51bbdb65e23b35c86e6a
Panda248 <p4nda248@gmail.com>
2023-02-05 14:43:47 -0500

    enemy not invis

--- Main.gd
+++ Main.gd
@@ -2,15 +2,18 @@ extends Node2D
 
 onready var bullet_manager = $BulletManager;
 onready var player = $Player;
+var tick = 0
 
 func _ready():
 	player.connect("player_shot", bullet_manager, "bullet_shot");
 
 
 func _process(delta):
+	tick += 1
 	for item in get_children():
 		if item.has_method("_dead"):
 			if item._dead():
 				remove_child(item)
-	
+	if tick%60 == 0:
+		add_child(get_node("Enemy"), true)
 	pass
