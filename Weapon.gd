532699c4a1a40cea1d42eb502aed14b8c6b13d86
Panda248 <p4nda248@gmail.com>
2023-02-09 02:21:39 -0500

    reloading and clips

--- Weapon.gd
+++ Weapon.gd
@@ -12,6 +12,13 @@ export var reserveClips := 2
 var shotTimeStamp = fireRate*-1
 var reloadAndShootDelay = 0
 
+func _process(delta):
+	if(self.reloadAndShootDelay > 0):
+		self.reloadAndShootDelay-=1
+	elif(currentClip == 0 && reserveClips > 0):
+		currentClip = clip
+		reserveClips-=1
+
 func primaryFire():
 	pass
 
