3a013e906e04b08ace89e6acdd9c3e8cc6692edb
Panda248 <p4nda248@gmail.com>
2023-02-11 19:33:16 -0500

    pistol sound

--- scripts/Pistol.gd
+++ scripts/Pistol.gd
@@ -14,7 +14,7 @@ func primaryFire():
 			var bullet_instance = Bullet.instance();
 			var target = get_global_mouse_position()
 			var direction_to_mouse = $BarrelEnd.global_position.direction_to(target).normalized();
-			
+			$AudioStreamPlayer2D.play(0)
 			get_parent().get_parent().emit_signal("player_shot", 
 												bullet_instance,
 												$BarrelEnd.global_position, 
