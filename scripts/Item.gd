e3664c4576d8a370989749b8216086a210632446
Lux <64970263+lumx7@users.noreply.github.com>
2023-02-19 20:39:09 -0500

    inventory

--- scripts/Item.gd
+++ scripts/Item.gd
@@ -1,12 +1,13 @@
 extends Area2D
 class_name Item
 
-
 var canInteract : bool = false
 var messageOffsetY = 50
 
 signal notify_picked_up(message, position, scale)
 
+func _ready():
+	$TextureRect.texture = load("res://res/exported/sprites/awp.png")
 
 func pick_up(player: Player):
 	emit_signal("notify_picked_up", "picked up", global_position + Vector2.UP*messageOffsetY, 0.5)
