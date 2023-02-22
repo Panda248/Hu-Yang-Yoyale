2a2cfd2bb30202ae432e362e32026d696ce1b601
Panda248 <p4nda248@gmail.com>
2023-02-22 08:24:44 -0500

    penetration var to bullet
    nothing done on it yet

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -3,9 +3,12 @@ extends Area2D
 var direction := Vector2.ZERO;
 
 export (int) var speed = 15;
+var penetration = 0;
 
 func _on_Bullet_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int):
-	queue_free()
+	if(penetration >= 0):
+		queue_free()
+	
 
 func _physics_process(delta: float) -> void:
 	if direction != Vector2.ZERO:
