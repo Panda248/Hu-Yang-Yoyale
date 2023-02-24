cca214f7df603ae70d913e313f4ca6ca6e6bee3e
Lux <64970263+lumx7@users.noreply.github.com>
2023-02-24 09:28:10 -0500

    cyclic dependency

--- scripts/Item.gd
+++ scripts/Item.gd
@@ -6,11 +6,35 @@ var canInteract : bool = false
 onready var radius = $CollisionShape2D.get_shape().radius
 onready var size = Vector2(radius*scale.x, radius*scale.y)
 
+var stack_size
+var item_name
+var item_type
+var item_quantity
+
 signal notify_picked_up(message, position, scale)
 
+func _ready():
+	item_type = "AMMO"
+	item_quantity = 1;
+	if item_type == "WEAPON":
+		stack_size = 1;
+	if item_type == "AMMO":
+		stack_size = 99;
+		
+	if stack_size == 1:
+		$Label.visible = false
+	$Label.text = String(item_quantity)
 
 func pick_up(player: Player):
 	if(canInteract):
-		owner = player
 		emit_signal("notify_picked_up", "picked up", global_position + Vector2.UP*messageOffsetY, 0.5)
 		queue_free()
+		
+func add_items(quantity):
+	item_quantity += quantity
+	$Label.text = String(item_quantity)
+
+func remove_items(quantity):
+	item_quantity -= quantity
+	$Label.text = String(item_quantity)
+
